3cec05c0ebf723d0493f3a7ae2985e7c84d6f445
Patricio de la Guardia <padlgt01@gmail.com>
2018-10-12 13:48:37 -0400

    Initial commit

---
+++ client/src/components/App.js
@@ -0,0 +1,29 @@
+// Import React as a dependency in order to be able
+// to use React
+import React from 'react';
+// Import axios as an http library
+import axios from 'axios';
+
+class App extends React.Component {
+	//constructor method that will set up our initial state
+	constructor() {
+		super();
+		this.state = {
+			recipes: [],
+			currentRecipe: {}
+		}
+	}
+
+	//Life cycle method that will be executed as soon
+	//as the component is mounted in the DOM
+	async componentDidMount() {
+		// Make a get request and store the data object of the 
+		// response in a local variable named data
+		let {data} = await axios.get('/recipes');
+		// Set the application state: 
+		// this.state.recipes = data
+		// this.state.currentRecipe = data[0]
+		this.setState({recipes: data, currentRecipe: data[0]});
+	}
+
+}
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//Simulate a Data Base
+const recipes = [{"name": "ham and cheese sandwich", "ingredients": "ham, cheese, bread", "description": "just a sandwich"}];
+
 // Imports express from node_modules
 const express = require('express');
 
@@ -11,6 +14,13 @@ const PORT = process.env.PORT || 5000;
 // Set up middleware function to serve static assets from
 // client/build directory, which contains optimized static files
 app.use(express.static(__dirname + '/client/build'));
+
+// API endpoints
+// GET /recipes: will send back the local recipes array
+app.get('/recipes', function(req, res) {
+	console.log("Ping!")
+	res.json(recipes);
+});
 
 // app will listen for incoming http requests on PORT
 app.listen(PORT, () => console.log(`App listening on ${PORT}`))
